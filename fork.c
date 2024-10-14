8b97c38a6b62f7e6c9770b75ee414c1d0a50f93f
prasadk <prasad.k@tapasi.com>
2024-10-14 22:46:30 +0530

    q

--- fork.c
+++ fork.c
@@ -4,16 +4,16 @@
 
 int main(){
 	int pid=fork();
-	if (pid>0){
+	// if (pid>0){
 		printf("I AKM PRENT PROCESS\n");
 		printf("ID:%d\n\n",getpid());
-	}
-	else if (pid==0){
-		printf("i am child process\n");
-		printf("ID:%d\n",getpid());
-	}
-	else{
-		printf("fe9ild to cearte a child process@s");
-	}
-	return 0;
+	// }
+	// else if (pid==0){
+	// 	printf("i am child process\n");
+	// 	printf("ID:%d\n",getpid());
+	// }
+	// else{
+	// 	printf("fe9ild to cearte a child process@s");
+	// }
+	// return 0;
 }
